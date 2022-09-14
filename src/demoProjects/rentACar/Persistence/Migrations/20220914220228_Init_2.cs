5f0f3e3a0c8441bc7b4e6536ff4d48c38ce42781
Çağrı KİBAR <77233335+CagriKibar@users.noreply.github.com>
2022-09-15 01:04:11 +0300

    Add project files.

---
+++ src/demoProjects/rentACar/Persistence/Migrations/20220914220228_Init_2.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class Init_2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SomeFeatureEntities",
+                table: "SomeFeatureEntities");
+
+            migrationBuilder.RenameTable(
+                name: "SomeFeatureEntities",
+                newName: "Brand");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Brand",
+                table: "Brand",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Brand",
+                table: "Brand");
+
+            migrationBuilder.RenameTable(
+                name: "Brand",
+                newName: "SomeFeatureEntities");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SomeFeatureEntities",
+                table: "SomeFeatureEntities",
+                column: "Id");
+        }
+    }
+}
